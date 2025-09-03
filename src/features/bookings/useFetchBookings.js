8a99027879b9dbcc0b8673c104e142e330641ee5
visheshtomarr <visheshtomar7800@gmail.com>
2025-09-03 22:47:15 +0530

    Added functionality to filter bookings

--- src/features/bookings/useFetchBookings.js
+++ src/features/bookings/useFetchBookings.js
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useFetchBookings() {
+    const [searchParams] = useSearchParams();
+
+    // Filter
+    const filteredValue = searchParams.get("status");
+    const filter = !filteredValue || filteredValue === "all" ? null : { field: "status", value: filteredValue };
+
     const { isLoading, data: bookings, error } = useQuery({
-        queryKey: ["bookings"],
-        queryFn: getBookings
+        queryKey: ["bookings", filter],
+        queryFn: () => getBookings({ filter })
     });
 
     return { isLoading, bookings, error };
